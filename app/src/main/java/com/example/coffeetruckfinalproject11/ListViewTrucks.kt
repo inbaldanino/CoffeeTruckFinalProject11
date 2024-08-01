0d177c23db7ee0466723162673e413c96bb8cdea
Eyal Haiman <pizzaeyal@gmail.com>
2024-08-01 22:20:00 +0300

    Fixed issues,
    Rows are not clickable, not getting the row number when clicking maybe because the frame layout

--- app/src/main/java/com/example/coffeetruckfinalproject11/ListViewTrucks.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject11/ListViewTrucks.kt
@@ -1,6 +1,7 @@
 package com.example.coffeetruckfinalproject11
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -32,6 +33,9 @@ class ListViewTrucks : Fragment() {
         trucks = Model.instance.trucks
         truckList = view.findViewById(R.id.lvTruckListView)
         truckList?.adapter = TruckListAdapter(trucks)
+        truckList?.setOnItemClickListener {parent, view, position, id ->
+            Log.i("TAG", "Row was click at: $position")
+        }
     }
     class TruckListAdapter (val trucks: MutableList<Truck>?): BaseAdapter() {
         override fun getCount(): Int = trucks?.size ?: 0
@@ -44,28 +48,34 @@ class ListViewTrucks : Fragment() {
             return position.toLong()
         }
 
+
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            var view: View? = null
-            if (convertView == null) {
-                view = LayoutInflater.from(parent?.context)
-                    .inflate(R.layout.truck_row_view, parent, false)
-            /*val checkBox:CheckBox? = view?.findViewById(R.id.cbRowTruckCheckBox)
-            checkBox?.apply { setOnClickListener{
-                trucks?. = checkBox.isChecked
-            } }*/
+            // Use convertView if it's not null, otherwise inflate a new view
+            val view: View = convertView ?: LayoutInflater.from(parent?.context)
+                .inflate(R.layout.truck_row_view, parent, false)
+
+            // Retrieve the Truck object for the current position
+            val truck = trucks?.get(position) ?: return view // Return early if truck is null
+
+            // Find views in the layout
+            val nameTextView: TextView = view.findViewById(R.id.tvRowTruckName)
+            val locationTextView: TextView = view.findViewById(R.id.tvRowTruckLocation)
+            val truckCheckBox: CheckBox = view.findViewById(R.id.cbRowTruckCheckBox)
+
+            // Set the text and checkbox state based on the Truck object
+            nameTextView.text = truck.name
+            locationTextView.text = truck.location
+            truckCheckBox.isChecked = truck.checkBox
+
+            // Update the Truck object when the checkbox state changes
+            truckCheckBox.setOnCheckedChangeListener { _, isChecked ->
+                truck.checkBox = isChecked
             }
-            val truck = trucks?.get(position)
-            val nameTextView: TextView? = view?.findViewById(R.id.tvRowTruckName)
-            val locationTextView: TextView? = view?.findViewById(R.id.tvRowTruckLocation)
-            val truckCheckBox : CheckBox? = view?.findViewById(R.id.cbRowTruckCheckBox)
 
-            nameTextView?.text = truck?.name
-            locationTextView?.text = truck?.location
-            truckCheckBox?.isChecked = truck?.checkBox ?: false
-
-            return view!!
-
+            return view
         }
+
+
     }
 }
 
