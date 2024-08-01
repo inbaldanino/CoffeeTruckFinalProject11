3a719d336c38ed95be2755820f5394b3a8fc297c
inbaldanino <Inbalino97@gmail.com>
2024-08-01 15:15:55 +0300

    functions for the addnewcoffeetruck frugment

--- app/src/main/java/com/example/coffeetruckfinalproject11/MainActivity.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject11/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.coffeetruckfinalproject11
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 /*class MainActivity : AppCompatActivity() {
     private var listViewFragment: ListViewTrucks? = null
@@ -82,30 +78,31 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             displayTruckListView()
         }
-    }
 
-    private fun onAddNewCoffeeTruckClicked() {
-        if (addNewCoffeeTruckFragment == null) {
-            displayAddNewCoffeeTruckFragment()
-        } else {
-            displayTruckListView()
-        }
+
+
+    }
+    //function for the addnewcoffeetruck frugment
+    private fun onAddNewCoffeeTruckClicked()
+    {
+        displayAddNewCoffeeTruckFragment()
+    }
+    private fun displayAddNewCoffeeTruckFragment()
+    {
+        val addNewCoffeeTruck = AddNewCoffeeTruck()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.flAddCoffeeTruck, addNewCoffeeTruck)
+        transaction.addToBackStack("Tag")
+        transaction.commit()
     }
 
     private fun displayTruckListView() {
         listViewFragment = ListViewTrucks()
         supportFragmentManager.beginTransaction()
-            .replace(R.id.flMainActivity, listViewFragment!!)
+            .replace(R.id.addNewCoffeeTruckButton, listViewFragment!!)
             .commit()
         addNewCoffeeTruckFragment = null
     }
 
-    private fun displayAddNewCoffeeTruckFragment() {
-        addNewCoffeeTruckFragment = AddNewCoffeeTruck()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.flMainActivity, addNewCoffeeTruckFragment!!)
-            .addToBackStack(null)
-            .commit()
-        listViewFragment = null
-    }
+
 }
