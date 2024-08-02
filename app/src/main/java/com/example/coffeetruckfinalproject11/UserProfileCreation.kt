c0d60cc89fae1d4f17d937cf141df992af23ca91
yaelw <yaelweiss164@gmail.com>
2024-08-02 14:10:21 +0300

    start

--- app/src/main/java/com/example/coffeetruckfinalproject11/UserProfileCreation.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject11/UserProfileCreation.kt
@@ -11,17 +11,13 @@ class UserProfileCreation : Fragment() {
 
 
 
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val View = inflater.inflate(R.layout.fragment_user_profile_creation, container, false)
-        return View
+        val view = inflater.inflate(R.layout.fragment_user_profile_creation, container, false)
+        return view
     }
-
-
 
 }
