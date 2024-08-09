e6982af050eca56c305077fd79a23a3d2d29408b
yaelw <yaelweiss164@gmail.com>
2024-08-09 18:21:48 +0300

    start to build the login page

--- app/src/main/java/com/example/coffeetruckfinalproject11/screens/auth/LoginFragment.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject11/screens/auth/LoginFragment.kt
@@ -1,6 +1,41 @@
 package com.example.coffeetruckfinalproject11.screens.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.coffeetruckfinalproject11.R
+import com.example.coffeetruckfinalproject11.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginFragment: Fragment () {
+class LoginFragment : Fragment() {
+
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNoAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/coffeetruckfinalproject11/screens/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject11/screens/auth/RegisterFragment.kt
@@ -1,6 +1,40 @@
 package com.example.coffeetruckfinalproject11.screens.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.coffeetruckfinalproject11.R
+import com.example.coffeetruckfinalproject11.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding: FragmentRegisterBinding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnHaveAccount.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
