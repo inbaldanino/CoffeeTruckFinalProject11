02245120c9e374c72d9c87cd4d29529bc3fd2979
gali2 <gulizha1123@gmail.com>
2024-08-16 17:15:19 +0300

    to move arg safely

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+buildscript {
+dependencies {
+val nav_version = "2.7.7"
+classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+}
+}
