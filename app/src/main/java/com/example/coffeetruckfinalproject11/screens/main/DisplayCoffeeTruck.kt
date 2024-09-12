40269315dbd682c508026d91213f31305014b799
Eyal Haiman <pizzaeyal@gmail.com>
2024-09-12 21:12:53 +0300

    fixed issues in DisplayCoffeeTruck

--- app/src/main/java/com/example/coffeetruckfinalproject11/screens/main/DisplayCoffeeTruck.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject11/screens/main/DisplayCoffeeTruck.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.coffeetruckfinalproject11.R
 import com.example.coffeetruckfinalproject11.databinding.FragmentDisplayCoffeeTruckBinding
@@ -23,19 +24,16 @@ class DisplayCoffeeTruck : Fragment() {
     private val binding: FragmentDisplayCoffeeTruckBinding get() = _binding!!
     private val params: DisplayCoffeeTruckArgs by navArgs()
 
-    private val viewModel : CoffeeTruckViewModel by activityViewModels()
-
-
+    private val viewModel :CoffeeTruckViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_coffee_truck, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+
         _binding = FragmentDisplayCoffeeTruckBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -43,12 +41,12 @@ class DisplayCoffeeTruck : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val truck = Gson().fromJson(params.truck, CoffeeTruck::class.java)
-        binding.textViewKosher.text = "כשרות: ${truck.kosher}"
-        binding.textViewLocation.text = "מיקום: ${truck.location}"
-        binding.textViewOpeningHours.text = " שעות פתיחה : ${truck.openingHours}"
-        binding.textViewName.text = "שם העגלה: ${truck.name}"
-        binding.textViewTripSuggestions.text = "המלצות: ${truck.tripSuggestions}"
-        binding.textViewRecommendations.text = "ביקורות: ${truck.recommendations}"
+        binding.textViewKosher.text = "Kosher: ${truck.kosher}"
+        binding.textViewLocation.text = "Location: ${truck.location}"
+        binding.textViewOpeningHours.text = "Opening hours: ${truck.openingHours}"
+        binding.textViewName.text = "Name: ${truck.name}"
+        binding.textViewTripSuggestions.text = "Suggestions: ${truck.tripSuggestions}"
+        binding.textViewRecommendations.text = "Recommendations: ${truck.recommendations}"
 
         val adapter = ArrayAdapter(
             requireContext(),
@@ -56,37 +54,35 @@ class DisplayCoffeeTruck : Fragment() {
             truck.reviews
         )
         binding.reviewsListView.adapter = adapter
-        binding.addReview.setOnClickListener()
-        {
+        binding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.addReview.setOnClickListener {
             val reviewView = layoutInflater.inflate(R.layout.review_layout, null, false)
             val reviewTv = reviewView.findViewById<EditText>(R.id.etReview)
-            val alertDialog = AlertDialog.Builder(requireContext())
+            android.app.AlertDialog.Builder(requireContext())
                 .setTitle("Add review for ${truck.name}")
                 .setView(reviewView)
-                .setPositiveButton("Submit") {_,_ ->
+                .setPositiveButton("Submit") { _, _ ->
                     val review = reviewTv.text.toString()
-                    if (review.isEmpty())
-                    {
+                    if (review.isEmpty()) {
                         Toast.makeText(
                             requireContext(),
-                            "Can not post an empty review",
+                            "Cannot post empty review",
                             Toast.LENGTH_LONG
                         ).show()
                     }
-                    else{
-                        viewModel.addReview(truck, review)
-
-                        {
-                            Toast.makeText(requireContext(),
-                                "Review was posted successfully",
-                                Toast.LENGTH_LONG)
-                                .show()
-                            adapter.add(review)
-                            //Will refresh the list
+                    else {
+                        viewModel.addReview(truck, review) {
+                            Toast.makeText(
+                                requireContext(),
+                                "Review posted successfully",
+                                Toast.LENGTH_LONG
+                            ).show()
                             adapter.notifyDataSetChanged()
                         }
+                    }
                 }
-        }
                 .setNegativeButton("Cancel", null)
                 .show()
         }
@@ -96,4 +92,5 @@ class DisplayCoffeeTruck : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
